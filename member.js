91777f0db641561b34867c66eac30015c583b0a0
dmorales <dmorales.a@gmail.com>
2024-05-10 10:34:21 -0600

    second

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMember() {
+  // Path: member.js
+  return {
+    // Path: member.js
+    skills: [
+      // Path: member.js
+      {
+        // Path: member.js
+        name: "JavaScript",
+        // Path: member.js
+        level: 5,
+      },
+      // Path: member.js
+      {
+        // Path: member.js
+        name: "React",
+        // Path: member.js
+        level: 4,
+      },
+    ],
+  };
+}
